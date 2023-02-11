3c5611c6499cf4b65cc4f4709d23775be16c5361
Callum Watkins <callum@callumwatkins.com>
2023-02-11 17:15:18 +0000

    Enable navigation between flow views

--- client/src/views/NewAgentView.vue
+++ client/src/views/NewAgentView.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
 import type NewAgentData from "@/models/NewAgentData";
-import type { Ref } from "vue";
+import { watch, type Ref } from "vue";
 import { ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+const flowRoutes = router
+  .getRoutes()
+  .find((r) => r.name === "new-agent")!.children;
+
+const currentFlowViewIndex = ref(-1);
+const transitionName = ref("");
+
+watch(
+  route,
+  () => {
+    currentFlowViewIndex.value = flowRoutes.findIndex(
+      (r) => r.name === route.name
+    );
+  },
+  { immediate: true }
+);
 
 const newAgentData: Ref<NewAgentData> = ref({
   created: new Date(),
@@ -13,25 +34,52 @@ const newAgentData: Ref<NewAgentData> = ref({
 });
 
 const canProceed = ref(false);
+
+function previousFlowView() {
+  transitionName.value = "slide-fade-inline-previous";
+  router.push({ name: flowRoutes[currentFlowViewIndex.value - 1].name });
+}
+
+function nextFlowView() {
+  transitionName.value = "slide-fade-inline-next";
+  router.push({ name: flowRoutes[currentFlowViewIndex.value + 1].name });
+}
 </script>
 
 <template>
   <section class="section">
     <div class="container">
       <div class="box">
-        <h1 class="title is-5">New Agent</h1>
         <div class="block">
+          <h1 class="title is-5">New Agent</h1>
           <RouterView
+            v-slot="{ Component }"
             :new-agent-data="newAgentData"
             @change="(done: boolean) => canProceed = done"
-          />
+          >
+            <Transition :name="transitionName" mode="out-in">
+              <component :is="Component" />
+            </Transition>
+          </RouterView>
         </div>
-        <div class="flow-buttons field is-grouped">
+        <div class="block spacer"></div>
+        <div class="block flow-buttons field is-grouped">
           <p class="control">
-            <button class="button">Back</button>
+            <button class="button" @click="previousFlowView">Back</button>
           </p>
           <p class="control">
-            <button class="button is-primary" :disabled="!canProceed">
+            <button
+              v-if="currentFlowViewIndex === flowRoutes.length - 1"
+              class="button is-primary"
+            >
+              Finish
+            </button>
+            <button
+              v-else
+              class="button is-primary"
+              :disabled="!canProceed"
+              @click="nextFlowView"
+            >
               Next
             </button>
           </p>
@@ -42,6 +90,40 @@ const canProceed = ref(false);
 </template>
 
 <style scoped lang="scss">
+.slide-fade-inline-previous-enter-active,
+.slide-fade-inline-previous-leave-active {
+  transition-property: opacity, transform;
+  transition-duration: 0.2s;
+  transition-timing-function: ease-in-out;
+}
+
+.slide-fade-inline-previous-enter-from {
+  opacity: 0;
+  transform: translateX(-20px);
+}
+
+.slide-fade-inline-previous-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.slide-fade-inline-next-enter-active,
+.slide-fade-inline-next-leave-active {
+  transition-property: opacity, transform;
+  transition-duration: 0.2s;
+  transition-timing-function: ease-in-out;
+}
+
+.slide-fade-inline-next-enter-from {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.slide-fade-inline-next-leave-to {
+  opacity: 0;
+  transform: translateX(-20px);
+}
+
 .title {
   text-align: center;
 }
@@ -52,9 +134,13 @@ const canProceed = ref(false);
 }
 
 .box {
+  min-height: 600px;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
   flex: 1;
   background-color: #303030;
   max-width: 700px;
+  padding: 2rem;
 }
 
 .flow-buttons {
