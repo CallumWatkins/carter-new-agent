b371423cd4eb0cdf68003b61497f6d99e9a75c28
Callum Watkins <callum@callumwatkins.com>
2023-02-11 13:58:24 +0000

    Create navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,54 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+const navOpen = ref(false);
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <RouterLink class="navbar-item" to="/">
+          <img
+            src="./assets/images/carter-full-white-transparent-cropped.svg"
+            width="112"
+            height="28"
+          />
+        </RouterLink>
+        <a
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': navOpen }"
+          aria-label="menu"
+          :aria-expanded="navOpen"
+          data-target="navbarBasicExample"
+          @click="navOpen = !navOpen"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': navOpen }"
+      >
+        <div class="navbar-start">
+          <RouterLink class="navbar-item" to="/">Home</RouterLink>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-light">Log out</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
