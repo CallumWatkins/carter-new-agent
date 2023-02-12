f68c9d83af06565e1843281838532f075fa55ae7
Callum Watkins <callum@callumwatkins.com>
2023-02-12 17:48:22 +0000

    Connect AgentView to agentsStore

--- client/src/views/AgentView.vue
+++ client/src/views/AgentView.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAgentsStore } from "@/stores/agents";
+import { useRoute } from "vue-router";
 
-<template><div></div></template>
+const agentsStore = useAgentsStore();
+const route = useRoute();
+const agent = agentsStore.agents.find((a) => a.id === route.params.id);
+</script>
+
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">{{ agent?.name }}</h1>
+      <h2 class="subtitle">Agent - {{ agent?.personality.join(", ") }}</h2>
+    </div>
+  </section>
+</template>
 
 <style scoped lang="scss"></style>
