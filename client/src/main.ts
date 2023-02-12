ebea2b2d5191fb686cad4e9804ea66427bf26ac5
Callum Watkins <callum@callumwatkins.com>
2023-02-12 15:04:59 +0000

    Uninstall FontAwesome

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,18 +6,9 @@ import router from "./router";
 
 import "./assets/styles/main.scss";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-import { faX, faPlus } from "@fortawesome/free-solid-svg-icons";
-
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faX, faPlus);
-
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.component("FontAwesomeIcon", FontAwesomeIcon);
 
 app.mount("#app");
